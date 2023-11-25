6310751c870865bc7ee9b32d6c85580dbdfd1d78
Michael Arnold <me@michaelarnold.io>
2023-11-24 22:21:51 -0800

    need to fix mysql impl

--- src/sql/ddl.sql
+++ src/sql/ddl.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS workout_tracker;
 USE workout_tracker;
+DROP TABLE IF EXISTS exercise_progress;
 CREATE TABLE IF NOT EXISTS exercise_progress (
   exercise ENUM('front-squat', 'deadlift', 'weighted-dip', 'barbell-curl', 'barbell-row', 'weighted-pullup', 'incline-dumbbell-press',  'bench-press', 'seated-db-press', 'overhead-press'),
-  date_recorded DATE,
+  date_recorded DATETIME,
   weight_in_lbs SMALLINT
 );
